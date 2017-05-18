d054d7725a368f135764e7205fdfc6ca4d7eaa40
Aaron Klump <sourcecode@intheloftstudios.com>
2017-05-18 10:31:31 -0700

    update build steps.

--- .web_package/hooks/build/2_demo_compile.sh
+++ .web_package/hooks/build/2_demo_compile.sh
@@ -3,5 +3,4 @@
 # @file
 # Copy relevent files from parent into demo
 #
-! test -e "$7/demo/jquery.loft_labels.js" || rm "$7/demo/jquery.loft_labels.js"
 cp "$7/jquery.loft_labels.js" "$7/demo/"
--- .web_package/hooks/build/9_dist.sh
+++ .web_package/hooks/build/9_dist.sh
@@ -3,10 +3,13 @@
 # @file
 # Copy distribution files to /dist
 # 
-sleep 3
+# Allow time for all CodeKit to minify.
+while [ ! -f "$7/jquery.loft_labels.min.js" ]; do
+  sleep 1
+done
 
-(! test -e "$7/dist" || rm -r "$7/dist") && mkdir -p "$7/dist"
+test -d "$7/dist" || mkdir "$7/dist"
 
 # Javascript
 cp "$7/jquery.loft_labels.js" "$7/dist/"
-test -e "$7/jquery.loft_labels.min.js" && cp "$7/jquery.loft_labels.min.js" "$7/dist/"
+cp "$7/jquery.loft_labels.min.js" "$7/dist/"
